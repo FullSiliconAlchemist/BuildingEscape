46ff665877d3b56283e5e540c003c7bfaa5f8b70
Charles Rabbat <charleskarim00@gmail.com>
2019-10-26 14:02:32 -0400

    implemented GetName() method to retrieve an objects name during LOG macro call

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PositionReport.h"
+
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -18,9 +19,12 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
+	// Seems like everything returns an object
+	FString name = GetOwner()->GetName();
+	
+	// UE_LOG is a macro
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting from %s"), *name);
 }
 
 
